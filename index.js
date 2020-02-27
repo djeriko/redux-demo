128b87dd3fb69ebe01596b15e46922f2048f061f
djeriko <jerikosalivan@gmail.com>
2020-02-27 16:40:09 +0900

    Multiple reducers

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
-const redux = require('redux')
+const redux = require("redux")
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
-const BUY_CAKE = "BUY_CAKE";
+const BUY_CAKE = "BUY_CAKE"
+const BUY_ICECREAM = "BUY_ICECREAM"
 
 // Action
 function buyCake() {
- return {
+  // action creator
+  return {
     type: BUY_CAKE,
-    info: "First redux aciton"
-  }
+    info: "First redux action"
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "Second redux action"
+  };
 }
 
 // State
-const initialState = {
+const initialCakeState = {
     numOfCakes: 10
+}
+
+const initialIceCreamState = {
+    numberOfIceCreams: 20
 }
 
 // Reducer
 // (previousState, aciton) => newState
-const reducer = (state = initialState, action) => {
-  switch(action.type){
-    case BUY_CAKE: return {
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
         ...state,
         numOfCakes: state.numOfCakes - 1
-    }
+      };
 
-    default: return state
+    default:
+      return state;
   }
 }
 
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams - 1
+      };
+
+    default:
+      return state;
+  }
+}
+
+// Multiple reducers
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
 // REDUX STORE
 // Holds application state
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 // Allows access to state via getState()
-console.log('Initial state', store.getState())
+console.log("Initial state", store.getState());
 
 // Registers listeners via subscribe(listeners)
-const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
 
 // Allows state to be updated via dispatch(action)
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
-unsubscribe()
-
+unsubscribe();
